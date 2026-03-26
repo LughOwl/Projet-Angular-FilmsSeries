b68877cb398247472a20d7b8a78de6332ffd9e86
hugov <hugovalente57930@gmail.com>
2026-03-26 13:23:00 +0100

    parametre fait

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {IonModal} from "@ionic/angular";
 
 @Component({
   selector: 'app-tabs',
@@ -8,6 +9,32 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
+  @ViewChild(IonModal) modal!: IonModal;
+
+  modeSombre: boolean = true;
+  couleurSelectionnee: string = 'orange';
+
+  codesCouleurs: { [key: string]: string } = {
+    'orange': '#e67e22',
+    'green': '#2ecc71',
+    'blue': '#3498db',
+    'purple': '#9b59b6'
+  };
+
   constructor() {}
 
+  // Change le mode (Sombre/Clair)
+  setMode(isSombre: boolean) {
+    this.modeSombre = isSombre;
+  }
+
+  // Change la couleur sélectionnée
+  setCouleur(couleur: string) {
+    this.couleurSelectionnee = couleur;
+    document.documentElement.style.setProperty('--main-color', this.codesCouleurs[couleur])
+  }
+
+  fermerParametres(){
+    this.modal.dismiss();
+  }
 }
